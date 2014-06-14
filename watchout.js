1fa5415786a2f3b5a9c40e692a9437078238facd
jamesyothers <james.yothers@gmail.com>
2014-06-14 16:28:53 -0700

    updating for merge

--- watchout.js
+++ watchout.js
@@ -1,2 +1,3 @@
 // start slingin' some d3 here.
 
+d3.selectAll('.gameboard').data([4,5,6,6,7,8]).enter().append('div').style('opacity', 0).text('hello world').transition().duration(1000).style('opacity', 1);
